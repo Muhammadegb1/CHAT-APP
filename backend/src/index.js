d1e41b92aaed6c3facd377909e27dc39bf3bb8d7
Muhammad_Egb <mhmad.m.ig2001@gmail.com>
2025-08-27 13:47:40 +0300

    add updateProfile and checkAuth controllers

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
+import cookieParser from "cookie-parser"
 
 
 dotenv.config();
@@ -9,9 +10,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cookieParser());
 app.use(express.json());
-app.use('/api/auth', authRoutes);
 
+app.use('/api/auth', authRoutes);
 
 
 app.listen(PORT, () => {
