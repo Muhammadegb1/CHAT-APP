09f99ebdfbac2b69b3adc4649b3535c836668a36
Muhammad_Egb <mhmad.m.ig2001@gmail.com>
2025-09-09 12:44:40 +0300

    Setup authentication store with Zustand and protect routes based on authUser

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from "cors"
+
 
 import authRoutes from './routes/auth.route.js';
 import messageRoutes from './routes/message.route.js';
@@ -15,6 +17,11 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors({
+  origin:"http://localhost:5173",
+  credentials: true
+}))
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/message', messageRoutes);
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 
-const Navbar = () => {
+ const Navbar = () => {
   return (
     <div>Navbar</div>
   )
